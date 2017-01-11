68c4bef2c44cde263e25e2e3c2d6a8009afb60c4
Dmitrii Kahmitov <khamitov.dm@gmail.com>
2017-01-11 09:43:50 +0300

    Test changed

--- src/test/java/d/kh/fun/thread/InterruptedExceptionTest.java
+++ src/test/java/d/kh/fun/thread/InterruptedExceptionTest.java
@@ -3,13 +3,24 @@ package d.kh.fun.thread;
 
 import org.junit.Test;
 
+import java.util.concurrent.LinkedBlockingQueue;
+
 /**
  * Created by khamitovdm on 11/01/2017.
  */
 public class InterruptedExceptionTest {
     @Test
-    public void interrupt_while_waiting() {
-        Thread thread = new Thread();
+    public void interrupt_while_waiting() throws InterruptedException {
+        LinkedBlockingQueue<Object> queue = new LinkedBlockingQueue<>();
+        Thread reader = new Thread(() -> {
+            try {
+                queue.take();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        reader.start();
 
+        reader.interrupt();
     }
 }
