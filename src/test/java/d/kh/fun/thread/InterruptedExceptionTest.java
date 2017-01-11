d58281ce143a1e0712ae5fb1096eff672cbb3890
Dmitrii Kahmitov <khamitov.dm@gmail.com>
2017-01-11 09:23:20 +0300

    Placeholder for test added

---
+++ src/test/java/d/kh/fun/thread/InterruptedExceptionTest.java
@@ -0,0 +1,15 @@
+package d.kh.fun.thread;
+
+
+import org.junit.Test;
+
+/**
+ * Created by khamitovdm on 11/01/2017.
+ */
+public class InterruptedExceptionTest {
+    @Test
+    public void interrupt_while_waiting() {
+        Thread thread = new Thread();
+
+    }
+}
