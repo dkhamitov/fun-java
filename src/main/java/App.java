2d020960ed04cb5dd14f977f08af4e23bd5189cd
Dmitrii Kahmitov <khamitov.dm@gmail.com>
2017-01-13 01:49:51 +0300

    Test updated

---
+++ src/main/java/App.java
@@ -0,0 +1,8 @@
+/*
+ * @author khamitovdm, @date 1/11/17 1:57 AM
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hola");
+    }
+}
--- src/test/java/d/kh/fun/thread/InterruptedExceptionTest.java
+++ src/test/java/d/kh/fun/thread/InterruptedExceptionTest.java
@@ -3,7 +3,11 @@ package d.kh.fun.thread;
 
 import org.junit.Test;
 
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * Created by khamitovdm on 11/01/2017.
@@ -11,14 +15,21 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class InterruptedExceptionTest {
     @Test
     public void interrupt_while_waiting() throws InterruptedException {
-        Thread reader = new Thread(() -> {
+        //given:
+        BlockingQueue<Object> signals = new LinkedBlockingQueue<>();
+        Thread hangingThread = new Thread(() -> {
             try {
                 new LinkedBlockingQueue<>().take();
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                signals.add(new Object());
             }
-        });
-        reader.start();
-        reader.interrupt();
+        }, "hanging-thread");
+        hangingThread.start();
+
+        //when:
+        hangingThread.interrupt();
+
+        //then:
+        assertNotNull("hangingThread should have been interrupted", signals.poll(100, MILLISECONDS));
     }
 }
