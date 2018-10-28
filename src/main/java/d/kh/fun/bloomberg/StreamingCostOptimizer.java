507e1db6cde7e48278a269b550952dfd2cf72b08
dkhamitov <khamitov.dm@gmail.com>
2018-10-29 00:10:10 +0300

    * ability to get top 10 combinations * small rewording and ˆrefactoring

--- src/main/java/d/kh/fun/bloomberg/StreamingCostOptimizer.java
+++ src/main/java/d/kh/fun/bloomberg/StreamingCostOptimizer.java
@@ -1,26 +1,33 @@
 package d.kh.fun.bloomberg;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 /**
- * Given N people and the prices for their travel to London and San Francisco, calculate
- * an optimal cost of sending half people to London and the other half to San Francisco. E.g.
+ * Given N people and the prices for their travel to London and San Francisco, calculate an optimal cost of sending
+ * half people to London and the other half to San Francisco. E.g. given the following table:
  * |Person|London|San Fra|
  * |P1    |100   |50     |
  * |P2    |200   |30     |
  * |P3    |370   |130    |
  * |P4    |40    |170    |
+ * <p>
+ * the solution should be:
+ * |Person|City   |
+ * |P1    |London |
+ * |P2    |San Fra|
+ * |P3    |San Fra|
+ * |P4    |London |
  */
 public class StreamingCostOptimizer {
     /*
-    N people. Each person can end up in either of 2 cities. So it's 2^N possible solutions of who should
-    fly where. But it doesn't take into account the requirement of splitting the group into halves. We
-    shouldn't consider the solutions which don't split the groups into halves.
-    There are only N/2 slots at the beginning for each city.
+    N people. Each person can end up in either of 2 cities. So it's 2^N possible solutions of who should fly where.
+    But it doesn't take into account the requirement of splitting the group into halves. We shouldn't consider the
+    solutions which don't split the groups into halves
 
-    Here is number representation of the problem, where 0 - London, 1 - San Francisco
+    Here is a numeric representation of the problem for a group consisting of 4 people. Where a, b, c, d are people
+    and 0 - London, 1 - San Francisco
+
+    abcd
 
     0000
     0001
@@ -42,54 +49,11 @@ public class StreamingCostOptimizer {
     1110
     1111
 
-    1100 is the normalized form of all the above solutions
+    As we can see, the selections are essentially so-called combinations of choosing 2 out of N
 
-    1a 1b 0  0
-    1a 0  1b 0
-    1a 0  0  1b
-
-    1b 1a 0  0  -
-    0  1a 1b 0
-    0  1a 0  1b
-
-    1b 0  1a 0  -
-    0  1b 1a 0  -
-    0  0  1a 1b
-
-    1b 0  0  1a -
-    0  1b 0  1a -
-    0  0  1b 1a -
-
-
-
-
-    L1 L2 SF SF
-    L1 SF L2 SF
-    L1 SF SF L2
-
-    L2 L1 SF SF -
-    SF L1 L2 SF
-    SF L1 SF L2
-
-    L2 SF L1 SF -
-    SF L2 L1 SF -
-    SF SF L1 L2
-
-    L2 SF SF L1 -
-    SF L2 SF L1 -
-    SF SF L2 L1 -
-
-    a b c d e
-      b c d e
-
-    a b
-    a c
-    a d
-    a e
-
-    N * (N - 1) = N! / (N - 2)!       - permutations, 2 out of N (or something - to check this in Cormen)
-    N * (N - (K - 1)) = N! / (N - K)! - permutations, general form of the above. It would be K out of N
-    N! / ((N - K)! * K!)              - combinations
+    N * (N - 1) = N! / (N - 2)!       - permutations, N choose 2
+    N * (N - (K - 1)) = N! / (N - K)! - permutations, general form of the above, N choose K
+    N! / ((N - K)! * K!)              - combinations, general form
 
     N * (N - 1) * (N - 2) * (N - (N/2 + 1)) = N! / (N - N/2)! - permutations
     N! / ((N - N/2)! * (N/2)!) = N! / ((N/2)! * (N/2)!)       - combinations
@@ -97,19 +61,12 @@ public class StreamingCostOptimizer {
     If N = 4 then
     4! / (2! * 2!) = 24 / 4 = 6
 
-    N! * N! = (1 * 2 * 3 * ... * (N - 1) * N) * (1 * 2 * 3 * ... * (N - 1) * N)
     (N^2)! > N! * N!
-
-    1307674368000
-    1.710012252724199e24
-    155117520.000000027289901
-    155 117 520
-    155117520
      */
     DatasetGenerator datasetGenerator;
     Timer timer;
 
-    public StreamingCostOptimizer(DatasetGenerator datasetGenerator, Timer timer) {
+    StreamingCostOptimizer(DatasetGenerator datasetGenerator, Timer timer) {
         this.datasetGenerator = datasetGenerator;
         this.timer = timer;
     }
@@ -122,11 +79,13 @@ public class StreamingCostOptimizer {
     }
 
     void go(String[] args) {
-        var ps = datasetGenerator.generate(30, 2);
-        var count = 20;
-        var timings = timer.time(() -> combinations(ps), count);
-        System.out.println(Arrays.toString(timings));
-
+//        var ps = datasetGenerator.generate(10, 2);
+        var ps = new int[][]{{100, 50}, {200, 30}, {370, 130}, {40, 170}};
+        var distrs = distributions(ps);
+        print(distrs, ps);
+//        var count = 20;
+//        var timings = timer.time(() -> distributions(ps), count);
+//        System.out.println(Arrays.toString(timings));
 
 //        var costs = new int[res.size()];
 //        for (var i = 0; i < res.size(); i++) {
@@ -156,42 +115,80 @@ public class StreamingCostOptimizer {
      * @param ps a table representing each person as a row with a price for each city as a column
      * @return an optimal cost
      */
-    int[][] combinations(int[][] ps) {
+    int[][] distributions(int[][] ps) {
         var n = ps.length;
         //the path shows how the tickets are distributed among the given people
-        var pathBuf = new int[n];
+        var distrBuf = new int[n];
         //maintains the path's summary so far, e.g. we need to stop calculation once we detect
         //that we miss a requirement. It can be a requirement of equal numbers of different values in the path.
         //For instance, the path 1010 contains two 1s and two 0s.
-        var pathBufSumm = new int[ps[0].length];
+        var distrBufSumm = new int[ps[0].length];
         //num - is a number of possible valid tickets' distribution among the people
-//        var num = fact(N) / (fact(N / 2) * fact(N / 2));
+        var num = fact(n) / (fact(n / 2) * fact(n / 2));
         //res - is a list of possible valid ticket's distribution among the people
-        var res = new ArrayList<int[]>();
-        run(ps, 0, pathBuf, pathBufSumm, res);
-        return res.toArray(new int[][]{{}});
+        var topDistrsNum = n < 10 ? n : 10;
+        var distrs = new int[topDistrsNum][];
+        var distrsCost = new int[topDistrsNum];
+        Arrays.fill(distrsCost, Integer.MAX_VALUE);
+        run(ps, 0, distrBuf, distrBufSumm, distrs, distrsCost);
+        return distrs;
     }
 
-    void run(int[][] ps, int k, int[] path, int[] pathSumm, List<int[]> res) {
-        if (failFast(ps, pathSumm)) {
+    int fact(int n) {
+        var fact = 1;
+        for (int i = 2; i <= n; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+
+    void run(int[][] ps, int k, int[] distr, int[] distrSumm, int[][] distrs, int[] distrsCost) {
+        if (failFast(ps, distrSumm)) {
             return;
         }
-        if (k == path.length) {
-            res.add(Arrays.copyOf(path, path.length));
+        if (k == distr.length) {
+            tryAddToTopDistrs(distrs, distrsCost, distr, ps);
         } else {
             var prices = ps[k];
             for (var i = 0; i < prices.length; i++) {
-                path[k] = i;
-                pathSumm[i]++;
-                run(ps, k + 1, path, pathSumm, res);
-                pathSumm[i]--;
+                distr[k] = i;
+                distrSumm[i]++;
+                run(ps, k + 1, distr, distrSumm, distrs, distrsCost);
+                distrSumm[i]--;
             }
         }
     }
 
-    boolean failFast(int[][] ps, int[] pathSumm) {
-        for (var i = 0; i < pathSumm.length; i++) {
-            if (pathSumm[i] > ps.length / 2) {
+    void tryAddToTopDistrs(int[][] distrs, int[] distrsCost, int[] distr, int[][] ps) {
+        var cost = calculateCost(distr, ps);
+        var pointcut = distrsCost.length;
+        while (pointcut > 0 && cost < distrsCost[pointcut - 1]) {
+            pointcut--;
+        }
+        if (pointcut < distrsCost.length) {
+            shiftArray(distrs, distrs.length, pointcut);
+            shiftArray(distrsCost, distrsCost.length, pointcut);
+            distrs[pointcut] = Arrays.copyOf(distr, distr.length);
+            distrsCost[pointcut] = cost;
+        }
+    }
+
+    int calculateCost(int[] distr, int[][] ps) {
+        var cost = 0;
+        for (var i = 0; i < distr.length; i++) {
+            var priceIndex = distr[i];
+            cost += ps[i][priceIndex];
+        }
+        return cost;
+    }
+
+    void shiftArray(Object array, int length, int shift) {
+        System.arraycopy(array, shift, array, shift + 1, length - shift - 1);
+    }
+
+    boolean failFast(int[][] ps, int[] distrSumm) {
+        for (var ds : distrSumm) {
+            if (ds > ps.length / 2) {
                 return true;
             }
         }
@@ -208,9 +205,7 @@ public class StreamingCostOptimizer {
     }
 
     void quickSort(int[] xs, int[] refs, int l, int r) {
-        if (l >= r) {
-            return;
-        } else {
+        if (l < r) {
             var pivot = l;
             for (var i = l + 1; i <= r; i++) {
                 var ref = refs[i];
@@ -227,17 +222,9 @@ public class StreamingCostOptimizer {
         }
     }
 
-//    private static int fact(int n) {
-//        var res = 1;
-//        for (int i = 2; i <= n; i++) {
-//            res *= i;
-//        }
-//        return res;
-//    }
-
-    void print(List<int[]> res, int[] costs) {
-        for (var i = 0; i < res.size(); i++) {
-            System.out.println(Arrays.toString(res.get(i)) + " " + costs[i]);
+    void print(int[][] distrs, int[][] ps) {
+        for (var distr : distrs) {
+            System.out.println(Arrays.toString(distr) + " " + calculateCost(distr, ps));
         }
     }
 }
