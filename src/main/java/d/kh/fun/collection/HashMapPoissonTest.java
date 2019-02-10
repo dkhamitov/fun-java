c989e65601b3412529564ff3895fb34955fd8a04
dkhamitov <khamitov.dm@gmail.com>
2019-02-11 01:21:15 +0300

    Check Poisson Distribution of HashMap buckets
    * print both ideal poisson and actual distributions

--- src/main/java/d/kh/fun/collection/HashMapPoissonTest.java
+++ src/main/java/d/kh/fun/collection/HashMapPoissonTest.java
@@ -3,15 +3,19 @@ package d.kh.fun.collection;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 import java.util.function.Supplier;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+import static java.lang.Math.exp;
+import static java.lang.Math.pow;
 import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toCollection;
+import static java.util.stream.Collectors.toMap;
 
 public class HashMapPoissonTest {
     public static void main(String[] args) {
@@ -33,6 +37,7 @@ public class HashMapPoissonTest {
 
         for (var rate : rates) {
             System.out.println("Testing with " + rate + " keys on average per bucket");
+            System.out.println(poisson(rate, 8) + " - Ideal Poisson Distribution");
             times(iters, () -> {
                 var map = new HashMap<String, Object>(cap);
                 times((int) (cap * rate), () -> {
@@ -43,7 +48,7 @@ public class HashMapPoissonTest {
                     map.put(key, null);
                 });
                 var table = (Object[]) readField(map, "table");
-                var distr = Arrays.stream(table).collect(groupingBy(bucket -> {
+                var counts = Arrays.stream(table).collect(groupingBy(bucket -> {
                     var bucketSize = 0;
                     while (bucket != null) {
                         bucketSize++;
@@ -51,9 +56,16 @@ public class HashMapPoissonTest {
                     }
                     return bucketSize;
                 }, counting()));
-                System.out.println(distr);
+                System.out.println(countsToDistr(counts, cap));
             });
+            System.out.println();
         }
+    }
+
+    private static Map<Integer, String> countsToDistr(Map<Integer, Long> counts, int cap) {
+        var distr = new HashMap<Integer, String>();
+        counts.forEach((size, count) -> distr.put(size, String.format("%.8f", (double) count / cap)));
+        return distr;
     }
 
     private static Object readField(Object obj, String name) {
@@ -73,6 +85,18 @@ public class HashMapPoissonTest {
             }
         }
         throw new IllegalArgumentException("Field not found");
+    }
+
+    private static Map<Integer, String> poisson(double rate, int count) {
+        return IntStream.range(0, count).boxed().collect(toMap(i -> i, i -> probability(rate, i)));
+    }
+
+    private static String probability(double rate, int i) {
+        return String.format("%.8f", exp(-rate) * pow(rate, i) / fact(i));
+    }
+
+    private static int fact(int n) {
+        return IntStream.rangeClosed(2, n).reduce(1, (l, r) -> l * r);
     }
 
     private static void times(int n, Runnable r) {
